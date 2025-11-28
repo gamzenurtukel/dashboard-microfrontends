d6c77457a2c17cfaa73705f48ac57b04a5d4c4c2
gamzenurtukel <gamzenurtukel@gmail.com>
2025-11-28 18:51:29 +0300

    style:css changes

--- packages/shell/src/components/Header.tsx
+++ packages/shell/src/components/Header.tsx
@@ -1,9 +1,7 @@
 const Header = () => {
   return (
-    <header className="p-4 shadow-md bg-gray-modern-100 dark:bg-gray-modern-800">
-      <h1 className="text-title-xl font-bold text-gray-modern-900 dark:text-gray-modern-100">
-        Dashboard Shell
-      </h1>
+    <header className="p-4 shadow-md bg-slate-900/60 border-b border-slate-800">
+      <h1 className="text-xl font-bold dark:text-white">Dashboard Shell</h1>
     </header>
   );
 };
